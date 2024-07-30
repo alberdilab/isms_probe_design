b0b3047a17592469d4bc210c131191f9cecb0c1b
Martí Dalmases <martidalmases@gmail.com>
2024-07-30 11:59:03 +0200

    Comments on concatenate

--- scripts/concatenate_fasta.py
+++ scripts/concatenate_fasta.py
@@ -3,13 +3,14 @@ import sys
 
 def concatenate_fasta(input_files, output_file):
 
-    # filter filter for .fa files
+    # filter for .fa files
     fasta_files = [f for f in input_files if f.endswith('.fa')]
 
     # raise an error if there are no .fa files
     if not fasta_files:
         raise FileNotFoundError("No FASTA files found in ./genomes/")
 
+    # concatenate each FASTA to output
     with open(output_file, 'w') as outfile:
         for file_path in fasta_files:
             with open(file_path, 'r') as infile:
